5b679621f4f264ef59e69c41ea08b5a333a18653
hannahhall <hannahkhall13@gmail.com>
2023-02-15 11:16:39 -0600

    start todo's

--- src/components/DecorationStation.js
+++ src/components/DecorationStation.js
@@ -1,3 +1,23 @@
+import './DecorationStation.css'
+
 export const DecorationStation = () => {
-  return <></>
+  // TODO: Create State for items, seasons, filteredItems, seasonChoice
+
+  // TODO: Create a useEffect that fetches items and seasons
+
+  // TODO: Create a useEffect that updates the filteredItems state if seasonChoice changes
+
+
+  return (
+    <>
+      <h1>Decoration Station</h1>
+      <div id="filter-bar">
+        {/* TODO: Create a select element that shows the seasons as options. Select should have an onChange function that sets the seasonChoice */}
+      </div>
+
+      <div className="item-container">
+        {/* TODO: Display the filteredItems */}
+      </div>
+    </>
+  )
 }
